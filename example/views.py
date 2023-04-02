351b44f198ca8af664aabe38fc84ae02bcc08368
mag <mag@planetfwd.com>
2023-04-02 15:04:27 -0700

    add supabase example

--- example/views.py
+++ example/views.py
@@ -1,16 +1,10 @@
 # example/views.py
 from datetime import datetime
-
 from django.http import HttpResponse
+from supabase import create_client, Client
+
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    supabase: Client = create_client('https://ymslwfbwjehfifxturei.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inltc2x3ZmJ3amVoZmlmeHR1cmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzMDQxOTMsImV4cCI6MTk5NTg4MDE5M30.Xwlun_ySomyoKIF1HgCzHal1tr09e6WsvNY6IQEzTB4')
+    response = supabase.table('test').select("id", "name").execute()
+    return HttpResponse(response.data)
