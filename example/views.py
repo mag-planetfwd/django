bfc2f3c30e03ef0fd11558bb62a080019d958933
mag <mag@planetfwd.com>
2023-04-03 05:25:27 -0700

    get supabase key and url from env

--- example/views.py
+++ example/views.py
@@ -1,9 +1,14 @@
 # example/views.py
 from django.http import HttpResponse
 from supabase import create_client, Client
+import os
+
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 
 
 def index(request):
-    supabase: Client = create_client('https://ymslwfbwjehfifxturei.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inltc2x3ZmJ3amVoZmlmeHR1cmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzMDQxOTMsImV4cCI6MTk5NTg4MDE5M30.Xwlun_ySomyoKIF1HgCzHal1tr09e6WsvNY6IQEzTB4')
+    supabase: Client = create_client(url, key)
     response = supabase.table('test').select("id", "name").execute()
     return HttpResponse(response.data, content_type='application/json')
