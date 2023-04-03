c8bd3f97a85f599574df8e73dcdc199aad79851f
mag <mag@planetfwd.com>
2023-04-02 21:37:22 -0700

    fix content type

--- example/views.py
+++ example/views.py
@@ -1,5 +1,4 @@
 # example/views.py
-from datetime import datetime
 from django.http import HttpResponse
 from supabase import create_client, Client
 
@@ -7,4 +6,4 @@ from supabase import create_client, Client
 def index(request):
     supabase: Client = create_client('https://ymslwfbwjehfifxturei.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inltc2x3ZmJ3amVoZmlmeHR1cmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzMDQxOTMsImV4cCI6MTk5NTg4MDE5M30.Xwlun_ySomyoKIF1HgCzHal1tr09e6WsvNY6IQEzTB4')
     response = supabase.table('test').select("id", "name").execute()
-    return HttpResponse(response.data)
+    return HttpResponse(response.data, content_type='application/json')
